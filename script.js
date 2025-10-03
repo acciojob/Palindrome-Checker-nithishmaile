b0bf436d314c8550fcabe728303f18bb4a9a0972
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-10-03 11:30:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
-function palindrome(str){
 
+function palindrome(str){
+	let reverseWord=""
+	for(let i=str.length-1;i>=0;i--){
+		reverseWord+=str[i];
+	}
+	return reverseWord
 }
 module.exports = palindrome
